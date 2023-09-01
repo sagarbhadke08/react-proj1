fbc6920074851e2ae518306fa1469af251d3c086
sagarbhadke08 <sagarbhadke08@gmail.com>
2023-09-01 23:06:31 +0530

    changed the Tagline

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1> Jai Shankar</h1>
+          Hey My Name is Sagar Bhadke 
+      
         </a>
       </header>
     </div>
