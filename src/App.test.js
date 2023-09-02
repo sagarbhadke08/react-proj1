eb8ebe864c62d07664e086d957d6681383dcd923
Sagar Bhadke <94558820+sagarbhadke08@users.noreply.github.com>
2023-09-02 22:52:51 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
+  //Tried Rendering concept.
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
